1a8445406365915dc7ab00adc7ddddb71ebcbd38
David Merrell <davidpmerrell@gmail.com>
2022-03-08 05:49:36 +0000

    More work on data simulation

--- src/simulate.jl
+++ src/simulate.jl
@@ -1,240 +1,339 @@
 
 
-function decompose_additive_variance(total_var, stn_ratio)
+#########################################
+# COMPUTE SIMULATION NOISE PARAMETERS
+#########################################
 
-    signal_var = total_var * stn_ratio / (1 + stn_ratio)
-    noise_var = total_var - signal_var
-    return signal_var, noise_var
-end
+function decompose_xor_signal(output_mean; 
+                              stn_ratio=10.0)
 
-
-function decompose_xor_variance(total_var, stn_ratio)
-
+    output_var = output_mean*(1 - output_mean)
     b = 1 + 1/stn_ratio
 
-    signal_var = (stn_ratio*0.125) * (b - sqrt(b^2 - 16*total_var/stn_ratio))
+    signal_var = (stn_ratio*0.125) * (b - sqrt(b^2 - 16*output_var/stn_ratio))
     noise_var = signal_var / stn_ratio
 
-    return signal_var, noise_var
+    signal_mean = 0.5*(1 - sqrt(1 - 4*signal_var))
+    noise_mean = 0.5*(1 - sqrt(1 - 4*signal_var))
+    return (signal_mean,), (noise_mean,)
 end
 
 
-function decompose_logistic_signal(output_mean, mtv_ratio)
+function decompose_logistic_signal(output_mean;
+                                   input_mtv=10.0)
 
-    #output_p = 0.5*(1 - sqrt(1 - 4*output_var))
-    input_mean = log(output_mean) * mtv_ratio / (mtv_ratio + 0.5)
-    input_var = input_mean / mtv_ratio
+    input_mean = log(output_mean) * input_mtv / (input_mtv + 0.5)
+    input_var = input_mean / input_mtv
 
-    return input_mean, input_var
+    return (input_mean, input_var)
 end
 
 
-function decompose_mult_variance(output_mean, output_var;
-                                 stn_ratio=1.0,
-                                 noise_mtv=1.0)
+function decompose_exp_signal(output_mean, output_var)
 
-    a = 1 - noise_mtv/stn_ratio
-    b = output_mean
-    c = - stn_ratio/(1 + noise_mtv)
+    M2 = output_mean*output_mean
 
-    signal_var = (-b + sqrt(b^2 - 4*a*c))/(2*a)
-    noise_var = signal_var/stn_ratio
-    
-    noise_mean = noise_mtv*noise_var
-    signal_mean = output_mean - noise_mean
+    input_mean = log(M2/sqrt(M2 + output_var))
+    input_var = log(1 + output_var/M2)
 
-    return signal_mean, signal_var, noise_mean, noise_var
+    return (input_mean, input_var)
 end
 
 
-function decompose_model_variance(out_var, out_mean;
-                                  theta_stn=10.0,
-                                  delta_stn=10.0,
-                                  mu_stn=10.0,
-                                  sigma_stn=10.0
+function decompose_additive_signal(output_mean, output_var;
+                                   stn_ratio=10.0,
+                                   noise_mtv=0.0)
+
+    signal_var = output_var * stn_ratio / (1 + stn_ratio)
+    noise_var = output_var - signal_var
+    noise_mu = sqrt(noise_mtv * noise_var)
+    signal_mu = output_mean - noise_mu
+    return (signal_mean, signal_var), (noise_mean, noise_var)
+end
+
+
+function decompose_mult_signal(output_mean, output_var;
+                               stn_ratio=1.0, 
+                               noise_mtv=1.0)
+
+    signal_var = sqrt((stn_ratio/(1 + noise_mtv))*(output_var - output_mean*output_mean/noise_mtv))
+    noise_var = signal_var / stn_ratio
+    noise_mean = sqrt(noise_mtv * noise_var)
+    signal_mean = output_mean/noise_mean
+
+    return (signal_mean, signal_var), (noise_mean, noise_var)
+end
+
+
+function decompose_matfac_signal(out_mean, out_var;
+                                 mu_stn=10.0,
+                                 delta_stn=10.0,
+                                 theta_stn=10.0,
                                  )
-    notheta_signal_var,
-    notheta_noise_var = decompose_additive_variance(out_var, 
-                                                    theta_stn)
+    notheta_signal,
+    theta_noise = decompose_additive_signal(out_mean, out_var;
+                                            stn_ratio=theta_stn,
+                                            noise_mtv=0.0) # noise is zero-mean
 
-    nodelta_signal_mean,
-    nodelta_signal_var,
-    nodelta_noise_mean,
-    nodelta_noise_var = decompose_mult_variance(out_mean, notheta_signal_var;
-                                                stn_ratio=delta_stn,
-                                                noise_mtv=1.0)
+    nodelta_signal,
+    delta_noise = decompose_mult_signal(notheta_signal[1], 
+                                        notheta_signal[2];
+                                        stn_ratio=delta_stn,
+                                        noise_mtv=1.0) # "noise" is exp-distributed
 
-    nomu_signal_var,
-    nomu_noise_var = decompose_additive_variance(nodelta_signal_var, mu_stn)
+    logdelta_noise = decompose_exp_signal(delta_noise[1],
+                                          delta_noise[2])
+
+    nomu_signal,
+    mu_noise = decompose_additive_signal(nodelta_signal[1],
+                                         nodelta_signal[2]; 
+                                         stn_ratio=mu_stn,
+                                         noise_mtv=0.0) # noise is zero-mean
    
     nosigma_signal_var = 1.0
-    nosigma_stn = nomu_signal_var / nosigma_signal_var
+    sigma_stn = nomu_signal[2] / nosigma_signal_var
   
+    nosigma_signal,
+    sigma_noise = decompose_mult_signal(nomu_signal[1], 
+                                        nomu_signal[2];
+                                        stn_ratio=sigma_stn,
+                                        noise_mtv=1.0) # "noise" is exp-distributed
+    
+    logsigma_noise = decompose_exp_signal(sigma_noise[1],
+                                          sigma_noise[2])
 
-
-    nosigma_signal_mean,
-    _,
-    nosigma_noise_mean,
-    nosigma_noise_var = decompose_mult_variance(nodelta_signal_mean, 
-                                                nomu_signal_var;
-                                                stn_ratio=sigma_stn,
-                                                noise_mtv=1.0)
-
-    return nosigma_noise_var, nomu_noise_var,  
-           nodelta_noise_var, notheta_noise_var 
+    return logsigma_noise, mu_noise,  
+           logdelta_noise, theta_noise 
 
 end
 
 
-function decompose_normal_data_variance(data_var, data_mean;
-                                        sample_stn=10.0,
-                                        theta_stn=10.0,
-                                        delta_stn=10.0,
-                                        mu_stn=10.0,
-                                        sigma_stn=10.0
-                                        )
+function decompose_normal_data_signal(data_mean, data_var;
+                               mu_stn=10.0,
+                               delta_stn=10.0,
+                               theta_stn=10.0,
+                               sample_stn=10.0,
+                               )
 
-    sample_signal_var,
-    sample_noise_var = decompose_additive_variance(data_var, sample_stn)
+    nosample_signal,
+    sample_noise = decompose_additive_signal(data_var, sample_stn)
 
-    nosigma_noise_var,
-    nomu_noise_var, 
-    nodelta_noise_var, 
-    notheta_noise_var = decompose_model_variance(sample_signal_var, data_mean;
-                                                 theta_stn=theta_stn,
-                                                 delta_stn=delta_stn,
-                                                 mu_stn=mu_stn,
-                                                 sigma_stn=sigma_stn
-                                               )
+    logsigma_noise,
+    mu_noise, 
+    logdelta_noise, 
+    theta_noise = decompose_matfac_signal(nosample_signal[1], 
+                                         nosample_signal[2];
+                                         theta_stn=theta_stn,
+                                         delta_stn=delta_stn,
+                                         mu_stn=mu_stn,
+                                         )
 
-    return nosigma_noise_var, nomu_noise_var,  
-           nodelta_noise_var, notheta_noise_var, 
-           sample_noise_var
+    return logsigma_noise, mu_noise,  
+           logdelta_noise, theta_noise, 
+           sample_noise
 end
 
 
-function decompose_bernoulli_data_variance(data_var;
-                                           logistic_mtv=10.0,
-                                           sample_stn=10.0,
-                                           theta_stn=10.0,
-                                           delta_stn=10.0,
-                                           mu_stn=10.0
-                                          )
+function decompose_bernoulli_data_signal(data_mean;
+                                  mu_stn=10.0
+                                  delta_stn=10.0,
+                                  theta_stn=10.0,
+                                  logistic_mtv=10.0,
+                                  sample_stn=10.0,
+                                  )
         
-    sample_signal_var,
-    sample_noise_var = decompose_xor_variance(data_var, sample_stn)
-
-    sample_signal_mean = 0.5*(1 - sqrt(1 - 4*sample_signal_var))
+    nosample_signal,
+    sample_noise = decompose_xor_signal(data_mean;
+                                        stn_ratio=sample_stn)
     
-    input_mean, 
-    input_var = decompose_logistic_signal(sample_signal_mean, logistic_mtv)
-    
-    nomu_noise_var, 
-    nodelta_noise_var, 
-    notheta_noise_var, 
-    notheta_noise_var = decompose_model_variance(input_var, input_mean;
-                                                 theta_stn=theta_stn,
-                                                 delta_stn=delta_stn,
-                                                 mu_stn=mu_stn,
-                                                 sigma_stn=sigma_stn
-                                                 )
+    input_signal = decompose_logistic_signal(nosample_signal[1];
+                                             input_mtv=logistic_mtv)
+   
+    logsigma_noise
+    mu_noise, 
+    logdelta_noise, 
+    theta_noise = decompose_matfac_signal(input_var, input_mean;
+                                          theta_stn=theta_stn,
+                                          delta_stn=delta_stn,
+                                          mu_stn=mu_stn,
+                                          )
 
-    return nosigma_noise_var, nomu_noise_var,  
-           nodelta_noise_var, notheta_noise_var, 
-           sample_noise_var
+    return logsigma_noise, mu_noise,  
+           logdelta_noise, theta_noise, 
+           sample_noise
 end
 
 
-#function normal_prec(precision::AbstractMatrix; n_samples=1)
-#
-#    N , _ = size(precision)
-#    if n_samples > 1
-#        z = CUDA.randn(N, n_samples)
-#    else
-#        z = CUDA.randn(N)
-#    end
-#    fac = cholesky(precision)
-#    x = fac.UP \ z
-#    
-#    return x
-#end
-#
-#
-#function X_sim_prior(reg_mats)
-#    K = length(reg_mats)
-#    return normal_prec(reg_mats[1]; n_samples=K) 
-#end
-#
-#
-#function Y_sim_prior(reg_mats)
-#    vecs = [normal_prec(mat) for mat in reg_mats]
-#    return hcat(vecs...)
-#end
-#
-#
-#function mu_sim_prior(reg_mat)
-#    return normal_prec(reg_mat)
-#end
-#
-#
-#function log_sigma_sim_prior(reg_mat)
-#    return normal_prec(reg_mat)
-#end
-#
-#
-#function theta_sim_prior(N_vec; std_theta=1.0)
-#    return BMFVec[CUDA.randn(N) .+ (randn()*std_theta) for N in N_vec]
-#end
-#
-#
-#function log_delta_sim_prior(N_vec; std_delta=1.0)
-#    return BMFVec[CUDA.randn(N) .+ (randn()*std_delta) for N in N_vec]
-#end
-#
-#
-#function simulate_params(X_reg, Y_reg, mu_reg, log_sigma_reg,
-#                         theta, log_delta)
-#   
-#    X = X_sim_prior(X_reg)
-#    Y = Y_sim_prior(Y_reg)
-#
-#    mu = mu_sim_prior(mu_reg)
-#    log_sigma = log_sigma_sim_prior(log_sigma_reg)
-#    
-#    N_vec = Int[length(v) for v in theta.values]
-#
-#    theta_copy = zero(theta)
-#    theta_copy.values = theta_sim_prior(N_vec)
-#    log_delta_copy = zero(log_delta) 
-#    log_delta_copy.values = log_delta_sim_prior(N_vec)
-#
-#    return ModelParams(X, Y, mu, log_sigma, theta_copy, log_delta_copy)
-#
-#end
-#
-#function simulate_data(X_reg, Y_reg, mu_reg, log_sigma_reg,
-#                       theta, log_delta, noise_models)
-#
-#    params = simulate_params(X_reg, Y_reg, mu_reg, log_sigma_reg,
-#                             theta, log_delta)
-#
-#    unq_nm = unique(noise_models)
-#    nm_ranges = ids_to_ranges(noise_models)
-#
-#    link_map = ColBlockMap(Function[LINK_FUNCTION_MAP[nm] for nm in unq_nm],
-#                           nm_ranges)
-#
-#    A = batch_forward(params.X, params.Y, params.mu, params.log_sigma,
-#                      params.theta, params.log_delta, link_map)
-#
-#    sampler_map = ColBlockMap(Function[SAMPLE_FUNCTION_MAP[nm] for nm in unq_nm],
-#                              nm_ranges)
-#
-#    D = sampler_map(A)
-#
-#    return params, D
-#    
-#end
+function decompose_all_data_signal(data_moments,
+                                   feature_batch_ids,
+                                   sample_batch_ids,
+                                   feature_losses;
+                                   mu_stn=10.0
+                                   delta_stn=10.0,
+                                   theta_stn=10.0,
+                                   logistic_mtv=10.0,
+                                   sample_stn=10.0)
+
+    unq_col_batches = unique(col_batch_ids)
+    col_ranges = ids_to_ranges(col_batch_ids)    
+    batch_losses = [unique(feature_losses[c_rng])[1] for c_rng in col_ranges]
+  
+    sig_decomp_map = Dict("normal"=>m -> decompose_normal_data_signal(m[1],m[2],
+                                                                      mu_stn=mu_stn,
+                                                                      delta_stn=delta_stn,
+                                                                      theta_stn=theta_stn,
+                                                                      sample_stn=sample_stn),
+                          "logistic"=>m -> decompose_bernoulli_data_signal(m[1],
+                                                                           mu_stn=mu_stn,
+                                                                           delta_stn=delta_stn,
+                                                                           theta_stn=theta_stn,
+                                                                           logistic_mtv=logistic_mtv,
+                                                                           sample_stn=sample_stn),
+                          "noloss"=>m -> decompose_normal_data_signal(0,1)
+                         )
+
+    # Compute variance contributions for each column batch
+    batch_characteristics = [sig_decomp_map[b_l](m) for (m, b_l) in zip(col_batch_moments, batch_losses)]
+
+    return batch_characteristics
+end
+
+#######################################
+# SIMULATE DATA
+#######################################
+
+function normal_prec(precision::AbstractMatrix; n_samples=1)
+
+    N , _ = size(precision)
+    if n_samples > 1
+        z = CUDA.randn(N, n_samples)
+    else
+        z = CUDA.randn(N)
+    end
+    fac = cholesky(precision)
+    x = fac.UP \ z
+    
+    return x
+end
+
+
+function sim_X(reg_mats, var)
+    K = length(reg_mats)
+    return normal_prec(reg_mats[1]; n_samples=K) 
+end
+
+
+function sim_Y(reg_mats, var)
+    vecs = [normal_prec(mat) for mat in reg_mats]
+    return hcat(vecs...)
+end
+
+
+function sim_mu(reg_mat, loc, var)
+    return normal_prec(reg_mat)*sqrt(var) + loc
+end
+
+
+function sim_log_sigma(reg_mat, loc, var)
+    return normal_prec(reg_mat)*sqrt(var) + loc
+end
+
+
+function sim_theta(N_vec, loc, var)
+    return BMFVec[CUDA.randn(N).*sqrt(var) .+ loc for N in N_vec]
+end
+
+
+function sim_log_delta(N_vec, loc, var)
+    return BMFVec[CUDA.randn(N).*sqrt(var) .+ loc for N in N_vec]
+end
+
+
+function simulate_params(X_reg, Y_reg,
+                         mu_reg, mu_noise,
+                         log_sigma_reg, log_sigma_noise,
+                         theta, theta_noise,
+                         log_delta, log_delta_noise)
+  
+    K = length(X_reg)
+
+    X = sim_X(X_reg)
+    Y = sim_Y(Y_reg) ./ sqrt(K)
+
+    mu = sim_mu(mu_reg, mu_noise[1], mu_noise[2])
+    log_sigma = sim_log_sigma(log_sigma_reg, 
+                              log_sigma_noise[1],
+                              log_sigma_noise[2])
+    
+    N_vec = Int[length(v) for v in theta.values]
+
+    theta_copy = zero(theta)
+    theta_copy.values = sim_theta(N_vec)
+    log_delta_copy = zero(log_delta) 
+    log_delta_copy.values = sim_log_delta(N_vec)
+
+    return ModelParams(X, Y, mu, log_sigma, theta_copy, log_delta_copy)
+
+end
+
+
+
+function simulate_data(params, noise_models)
+
+    unq_nm = unique(noise_models)
+    nm_ranges = ids_to_ranges(noise_models)
+
+    link_map = ColBlockMap(Function[LINK_FUNCTION_MAP[nm] for nm in unq_nm],
+                           nm_ranges)
+
+    A = batch_forward(params.X, params.Y, params.mu, params.log_sigma,
+                      params.theta, params.log_delta, link_map)
+
+    sampler_map = ColBlockMap(Function[SAMPLE_FUNCTION_MAP[nm] for nm in unq_nm],
+                              nm_ranges)
+
+    D = sampler_map(A)
+
+    return params, D
+    
+end
+
+
+function full_simulation(X_reg, Y_reg,
+                         col_batch_moments,
+                         feature_batch_ids,
+                         sample_batch_ids,
+                         feature_losses;
+                         mu_stn=10.0
+                         delta_stn=10.0,
+                         theta_stn=10.0,
+                         logistic_mtv=10.0,
+                         sample_stn=10.0)
+
+
+    # Compute the contributions of different model
+    # parameters to the data's overall means and variances
+    batch_var_contribs = decompose_all_data_signal(data_moments,
+                                                   feature_batch_ids,
+                                                   sample_batch_ids,
+                                                   feature_losses;
+                                                   mu_stn=mu_stn,
+                                                   delta_stn=delta_stn,
+                                                   theta_stn=theta_stn,
+                                                   logistic_mtv=logistic_mtv,
+                                                   sample_stn=sample_stn)
+
+    # Simulate model parameters
+    sim_model_params = simulate_params(X_reg, Y_reg, 
+                                       feature_batch_ids,
+                                       sample_batch_ids,
+                                       batch_var_contribs)
+
+    # Simulate the data from the parameters
+   
+    # Return the parameters and data
+end
+
 
 
 
